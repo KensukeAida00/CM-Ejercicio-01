17132167f0010716698c75cd05061ca9d8a368de
Roger Aquino <kensukeaida00@gmail.com>
2020-11-23 10:24:14 +0000

    capacity by count

--- main.swift
+++ main.swift
@@ -22,8 +22,7 @@ func conAlumno(i:Int)->(String,Int,Double,Bool){
 }
 func printAll(alumnes:[alumno]){
   var i:Int=0
-  var larg: Int=alumnes.capacity-1
-  if larg==0{larg=1}
+  let larg:Int=(alumnes.count)
   while i<larg{
     print("Alumno",i+1,
           "\nNombre",alumnes[i].n,
@@ -63,23 +62,23 @@ func reNombres(alumnes:[alumno],temp:[String],larg: Int)->[alumno]{
   var i: Int=0
   var j: Int=0
   var alumnes2: [alumno]=[]
-    i=0
-    while i<larg{
-      while j<larg{
-        if temp[i]==alumnes[j].n{
-          alumnes2.append(alumnes[j])
-          j=larg
-          }
-        else {j=j+1}
-      }
-      j=0
-      i=i+1
+  i=0
+  while i<larg{
+    while j<larg{
+      if temp[i]==alumnes[j].n{
+        alumnes2.append(alumnes[j])
+        j=larg
+        }
+      else {j=j+1}
     }
-    return alumnes2
+    j=0
+    i=i+1
+  }
+  return alumnes2
 }
 func alfabetico(alumnes:[alumno])->[alumno]{
     var i: Int=0
-    let larg: Int=alumnes.capacity-1
+    let larg: Int=alumnes.count
     var temp: [String]=[]
     while i<larg{
       temp.append(alumnes[i].n)
@@ -94,15 +93,14 @@ func pExtremos(alumnes:[alumno],ext:Double,larg: Int)->[alumno]{
   while j<larg{
     if ext==alumnes[j].p{
       alumnes2.append(alumnes[j])
-      j=larg
     }
-    else {j=j+1}
+    j=j+1
   }
   return alumnes2
 }
 func promedios(alumnes:[alumno])->([alumno],[alumno]){
   var i: Int=0
-  let larg: Int=alumnes.capacity - 1
+  let larg: Int=alumnes.count
   var temp: [Double]=[]
   while i<larg{
     temp.append(alumnes[i].p)
@@ -115,18 +113,28 @@ func promedios(alumnes:[alumno])->([alumno],[alumno]){
 func becarios(alumnes:[alumno])->[alumno]{
   var j: Int=0
   var alumnes2: [alumno]=[]
-  let larg: Int=alumnes.capacity - 1
+  let larg: Int=alumnes.count
   while j<larg{
     if true==alumnes[j].b{
       alumnes2.append(alumnes[j])
-      j=larg
     }
-    else {j=j+1}
+    j=j+1
   }
   return alumnes2
 }
 var alumnes: [alumno]=[]
-alumnes=intome()
+//alumnes=intome()
+alumnes.append(alumno(n:"Roberto",e:15,p:9.5,b:true))
+alumnes.append(alumno(n:"Kenia",e:19,p:10.0,b:true))
+alumnes.append(alumno(n:"Katia",e:24,p:7.5,b:false))
+alumnes.append(alumno(n:"Andrea",e:22,p:10.0,b:true))
+alumnes.append(alumno(n:"Gonzalo",e:24,p:6.0,b:false))
+alumnes.append(alumno(n:"Diego",e:26,p:6.0,b:true))
+alumnes.append(alumno(n:"Ricardo",e:21,p:8.9,b:false))
+alumnes.append(alumno(n:"Sebastian",e:19,p:7.6,b:false))
+alumnes.append(alumno(n:"Kevin",e:18,p:6.1,b:true))
+alumnes.append(alumno(n:"Ernesto",e:24,p:7.4,b:false))
+alumnes.append(alumno(n:"Sandra",e:22,p:6.6,b:true))
 print("Alumnos ingresados")
 printAll(alumnes:alumnes)
 print("Ordenando Alfabeticamente")
